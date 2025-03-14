f97d7b54524c6fd227b7fca5ec0ea28f9cb1292c
unknown <md031194@gmail.com>
2025-03-14 00:46:22 -0300

    fix: problems with dom add appenchild

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const form = document.querySelector('form')
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const spanContent = document.getElementById('content')
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const spanContent = document.querySelector("ul");
+const newElement = document.createElement("li");
 
-form.addEventListener("submit", (event)=> {
-    event.defaultPrevented()
-    spanContent.innerText = "teste"
-    
-})
+input.addEventListener("input", () => {
+  const hashcCodeRegex = /[^a-z]+/g;
+  input.value = input.value.replace(hashcCodeRegex, "");
+});
 
+newElement.innerHTML =
+  '<input type="checkbox"/><span>teste</span><a href=""><i class="hgi hgi-stroke hgi-delete-02"></i></a>';
 
-
+spanContent.appendChild(newElement);
