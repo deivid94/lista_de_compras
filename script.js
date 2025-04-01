26b5ed15919d60428670a6d57e8ffa21e8c1c02d
unknown <md031194@gmail.com>
2025-04-01 01:42:52 -0300

    refactory: abstracion codes

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
 const inputUser = document.querySelector("input");
-
 const button = document.querySelector("button");
 const spanContent = document.querySelector("ul");
 const a = document.querySelector("a");
+const checkboxList = document.querySelector("input[type=checkbox]");
 
-inputUser.addEventListener("input", () => {
-  const hashcCodeRegex = /[^a-z ]+/gi;
-  // const hashcCodeRegex = /^(\w+ ?)*$/
-
-  inputUser.value = inputUser.value.replace(hashcCodeRegex, "");
-});
-
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  const newElement = document.createElement("li");
-  const input = document.createElement("input");
-  const span = document.createElement("span");
-
-  input.type = "checkbox";
-
-  span.innerText = `${inputUser.value}`;
-
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerHTML = `<i class="hgi hgi-stroke hgi-delete-02"></i>`;
-  a.classList.add("delete");
-  newElement.classList.add("label", "margin-botton");
-
-  newElement.appendChild(input);
-  newElement.appendChild(span);
-  newElement.appendChild(a);
-
-  // newElement.innerHTML = `<input type="checkbox"/> <span>${input.value}</span><a href=""><i class="hgi hgi-stroke hgi-delete-02"></i></a>`;
-  spanContent.appendChild(newElement);
-
-  const checkboxList = document.querySelector("input[type=checkbox]");
-  const deleteItemList = (document.getElementsByClassName("delete").onclick = (
-    e
-  ) => {
-    alert("item deletado");
-  }); // resolver deletar item
-
-  checkboxList.addEventListener("change", (e) => {
-    if (e.target.checked) {
-      alert("caixa marcada");
-    }
+function InputItemListaUsuario() {
+  inputUser.addEventListener("input", () => {
+    const hashcCodeRegex = /[^a-z ]+/gi;
+    inputUser.value = inputUser.value.replace(hashcCodeRegex, "");
+    return inputUser.value;
   });
-});
+}
+
+function CriaElementoHtml() {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const newElement = document.createElement("li");
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    const span = document.createElement("span");
+
+    span.innerText = `${inputUser.value}`;
+    inputUser.value = "";
+    const a = document.createElement("a");
+    a.href = "#";
+    a.innerHTML = `<i class="hgi hgi-stroke hgi-delete-02"></i>`;
+    a.classList.add("delete");
+    newElement.classList.add("label", "margin-botton");
+
+    newElement.appendChild(input);
+    newElement.appendChild(span);
+    newElement.appendChild(a);
+    spanContent.appendChild(newElement);
+  });
+}
+
+function DeleteItemList() {
+  const iconDelete = document.querySelector("delete");
+  iconDelete.addEventListener("click", (e) => {
+    alert("ouviu o eevento");
+    spanContent.remove();
+  });
+}
+
+InputItemListaUsuario();
+CriaElementoHtml();
+DeleteItemList();
